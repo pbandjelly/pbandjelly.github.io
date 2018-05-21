a048dd0d5230e75e08fbc4556b15ced1fbcebf05
James K. Park <james@Jamess-MacBook-Pro.local>
2018-05-21 14:43:41 -0400

    more stuff

---
+++ posts/0.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>James Park</title>
+
+		<!-- meta -->
+		<meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	    <!-- css -->
+		<link rel="stylesheet" href="../css/bootstrap.min.css">
+		<link rel="stylesheet" href="../css/ionicons.min.css">
+		<link rel="stylesheet" href="../css/pace.css">
+	    <link rel="stylesheet" href="../css/custom.css">
+
+	    <!-- js -->
+	    <script src="../js/jquery-2.1.3.min.js"></script>
+	    <script src="../js/bootstrap.min.js"></script>
+	    <script src="../js/pace.min.js"></script>
+	    <script src="../js/modernizr.custom.js"></script>
+	</head>
+
+	<body>
+		<div class="container">
+			<header id="site-header">
+				<div class="row">
+					<div class="col-md-4 col-sm-5 col-xs-8">
+						<div class="logo">
+							<h1><a href="../index.php"><b>James Park</b></a></h1>
+						</div>
+					</div><!-- col-md-4 -->
+					<div class="col-md-8 col-sm-7 col-xs-4">
+						<nav class="main-nav" role="navigation">
+							<div class="navbar-header">
+  								<button type="button" id="trigger-overlay" class="navbar-toggle">
+    								<span class="ion-navicon"></span>
+  								</button>
+							</div>
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+  								<ul class="nav navbar-nav navbar-right">
+    								<li class="cl-effect-11"><a href="../index.php" data-hover="Blog">Blog</a></li>
+    								<li class="cl-effect-11"><a href="../about.php" data-hover="About">About</a></li>
+  								</ul>
+							</div><!-- /.navbar-collapse -->
+						</nav>
+					</div><!-- col-md-8 -->
+				</div>
+			</header>
+		</div>
+
+		<div class="content-body">
+			<div class="container">
+				<div class="row">
+					<main class="col-md-12">
+						<article class="post post-0">
+							<header class="entry-header">
+								<h1 class="entry-title" id="postheader">First post</h1>
+								<div class="entry-meta">
+									<span class="post-date"><a href="#"></a></span>
+									<span class="post-date"><a href="#"><time class="entry-date">May 21, 2018</time></a></span>
+								</div>
+							</header>
+							<div class="entry-content clearfix">
+								<p>Post text 0</p>
+							</div>
+						</article>
+					</main>
+				</div>
+			</div>
+		</div>
+		<footer id="site-footer">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<p class="copyright">&copy; All rights reserved.</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+		<!-- Mobile Menu -->
+		<div class="overlay overlay-hugeinc">
+			<button type="button" class="overlay-close"><span class="ion-ios-close-empty"></span></button>
+			<nav>
+				<ul>
+					<li><a href="../index.php">Blog</a></li>
+					<li><a href="../about.php">About</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<script src="../js/script.js"></script>
+
+	</body>
+</html>
